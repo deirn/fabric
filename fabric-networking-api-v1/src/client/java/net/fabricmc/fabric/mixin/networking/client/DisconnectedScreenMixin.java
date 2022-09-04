b605e5fd9e9f9c17c6011df2777f7d23dcf04971
deirn <deirn@bai.lol>
2022-09-05 00:03:46 +0700

    Fix disconnected screen scissoring on high dpi screens

--- fabric-networking-api-v1/src/client/java/net/fabricmc/fabric/mixin/networking/client/DisconnectedScreenMixin.java
+++ fabric-networking-api-v1/src/client/java/net/fabricmc/fabric/mixin/networking/client/DisconnectedScreenMixin.java
@@ -16,7 +16,6 @@
 
 package net.fabricmc.fabric.mixin.networking.client;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -64,10 +63,9 @@ public abstract class DisconnectedScreenMixin extends Screen {
 
 	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/MultilineText;drawCenterWithShadow(Lnet/minecraft/client/util/math/MatrixStack;II)I"))
 	private int render(MultilineText instance, MatrixStack matrixStack, int x, int y) {
-		double scale = client.getWindow().getScaleFactor();
-		RenderSystem.enableScissor(0, (int) (y * scale), (int) (width * scale), (int) (reasonHeight * scale));
+		enableScissor(0, y, width, y + reasonHeight);
 		instance.drawCenterWithShadow(matrixStack, x, y - scroll);
-		RenderSystem.disableScissor();
+		disableScissor();
 
 		// Draw gradient at the top/bottom to indicate that the text is scrollable.
 		if (actualReasonHeight > reasonHeight) {
